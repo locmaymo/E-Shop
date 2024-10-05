d6285c5f8dfe98a1b975d9a2ef2180b920cb65c9
Restyled.io <commits@restyled.io>
2024-10-05 04:03:40 +0000

    Restyled by clang-format

--- EShop/src/Services/Post/Post.API/Models/Image.cs
+++ EShop/src/Services/Post/Post.API/Models/Image.cs
@@ -1,31 +1,17 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-namespace Post.API.Models
-{
-public class Image
-{
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id {
-        get;    // MongoDB will auto-generate this if not set
+namespace Post.API.Models {
+public class Image {
+  [BsonId]
+  [BsonRepresentation(BsonType.ObjectId)]
+  public string Id {
+  get; // MongoDB will auto-generate this if not set
         set;
     }
-    public string Name {
-        get;
-        set;
-    }
-    public string Url {
-        get;
-        set;
-    }
-    public string Description {
-        get;
-        set;
-    }
-    public DateTime CreatedAt {
-        get;
-        set;
-    }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
 }
 }
--- EShop/src/Services/Post/Post.API/WeatherForecast.cs
+++ EShop/src/Services/Post/Post.API/WeatherForecast.cs
@@ -1,22 +1,11 @@
-namespace Post.API
-{
-public class WeatherForecast
-{
-    public DateTime Date {
-        get;
-        set;
-    }
+namespace Post.API {
+public class WeatherForecast {
+  public DateTime Date { get; set; }
 
-    public int TemperatureC {
-        get;
-        set;
-    }
+  public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary {
-        get;
-        set;
-    }
+  public string? Summary { get; set; }
 }
 }
